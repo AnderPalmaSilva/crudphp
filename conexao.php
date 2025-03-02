1703ed451c73c8581d00c0247793a6af5150d256
Anderson da Silva Palma <46480254+AnderPalmaSilva@users.noreply.github.com>
2025-03-02 17:29:42 -0400

    Subindo projeto
    Subindo para o github

---
+++ conexao.php
@@ -0,0 +1,6 @@
+<?php
+define('HOST', '127.0.0.1');
+define('USUARIO', 'root');
+define('SENHA', 'root');
+define('DB', 'crudphp');
+$conexao = mysqli_connect(HOST, USUARIO, SENHA, DB) or die('Não foi possível conectar');
